fc0961cd0d232dcd333eaef206f4020350a26b02
jcs1910 <englishtour0224@gmail.com>
2022-04-14 17:05:42 +0900

    conf/first_settings

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['media.graphcms.com']
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack']
-    })
-    return config
-  }
+  reactStrictMode: true
 }
