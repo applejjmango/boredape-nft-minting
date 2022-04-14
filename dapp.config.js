4dd25f2751cda721752fb48e35dd7cfec177d3fd
jcs1910 <englishtour0224@gmail.com>
2022-04-14 16:52:20 +0900

    feat/front-end 기본 레이아웃 작업 완료

---
+++ dapp.config.js
@@ -0,0 +1,8 @@
+export const config = {
+  title: 'BoredApe Dapp',
+  description: 'A showcase dapp which is built for you to create your own',
+  contractAddress: '',
+  maxMintAmount: 10,
+  presaleMaxMintAmount: 3,
+  price: 0.1
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['media.graphcms.com'],
+    domains: ['media.graphcms.com']
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    })
+    return config
+  }
 }
